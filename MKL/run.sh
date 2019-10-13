435dcefb50dcc93d63825c0fb229fd22e2dfeddb
Tung <tung.huynh@seedcom.vn>
2019-10-13 18:01:55 -0500

    [UPDATE] MKL script

--- MKL/run.sh
+++ MKL/run.sh
@@ -15,35 +15,28 @@ rm PAPI_*.txt
 for THREAD in ${THREADS[*]}; do
     export MKL_NUM_THREADS=$THREAD
     for S in ${NSIZES[*]}; do
-        OUTER_THREADS_NAME=OUTER_THREADS_${THREAD}
-        eval "OUTER_THREADS=(\"\${${OUTER_THREADS_NAME}[@]}\")"
-        # OUTER_THREADS=${!OUTER_THREADS_NAME[@]}
-        # echo ${OUTER_THREADS[@]}
-        for OUTER_T in ${OUTER_THREADS[*]}; do
-            INNER_T=$((THREAD/OUTER_T))
-            printf "\n\n\n"
-            echo "THREAD=$THREAD;SIZE=$S;OUTER=$OUTER_T;INNER=$INNER_T"
-            echo "COMPILE---------------------------------------"
-            echo "Using optimization MKL"
-            tau_cc.sh -fopenmp -I$MKLROOT/include         \
-                    -Wl,-L${MKLROOT}/lib/intel64     \
-                    -lmkl_intel_lp64 -lmkl_core      \
-                    -lmkl_gnu_thread -lpthread       \
-                    -lm -ldl mklMul.c -o matmul		\
-                    -DSIZE=$S > compile.out
-            echo "PAPI--------------------"
-            sh ./papi_event.sh
-            echo "SUMMARY-----------------------------------------"
-            tau_exec -T OPENMP ./matmul
-            echo "---------------------------"
-            pprof
-            echo "RAPL"
-            echo "----------------------------"
-            icc -Wall -O2 rapl-read.c -o rapl-read
-            ./rapl-read | grep package-0
-            ./rapl-read | grep package-0
-            ./rapl-read | grep package-0
-        done
+        printf "\n\n\n"
+        echo "THREAD=$THREAD;SIZE=$S;"
+        echo "COMPILE---------------------------------------"
+        echo "Using optimization MKL"
+        tau_cc.sh -fopenmp -I$MKLROOT/include         \
+                -Wl,-L${MKLROOT}/lib/intel64     \
+                -lmkl_intel_lp64 -lmkl_core      \
+                -lmkl_gnu_thread -lpthread       \
+                -lm -ldl mklMul.c -o matmul		\
+                -DSIZE=$S > compile.out
+        echo "PAPI--------------------"
+        sh ./papi_event.sh
+        # echo "SUMMARY-----------------------------------------"
+        # tau_exec -T OPENMP ./matmul
+        # echo "---------------------------"
+        # pprof
+        echo "RAPL"
+        echo "----------------------------"
+        icc -Wall -O2 rapl-read.c -o rapl-read
+        ./rapl-read | grep package-0
+        ./rapl-read | grep package-0
+        ./rapl-read | grep package-0
     done
 done
 
